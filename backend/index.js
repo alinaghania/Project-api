8850735d4369b9df3c3f84000c113afa3871bcd8
Alina Ghani <alina@MacBook-Air-de-Alina.local>
2023-07-15 23:00:31 +0200

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:8080',
+  credentials: true
+}));
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Add the Permissions-Policy header middleware
+app.use((req, res, next) => {
+  res.setHeader('Permissions-Policy', 'ch-ua-platform=*, ch-ua-platform-version=*, ch-ua-model=*, ch-ua-mobile=*');
+
+  next();
+});
 
 // Connect to MongoDB
 mongoose
@@ -15,6 +33,60 @@ mongoose
   })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+// Middleware express-session
+app.use(
+  session({
+    secret: 'votre_secret_key',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// Initialisation de Passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Sérialisation et désérialisation de l'utilisateur
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Google authentication strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: '935153154949-h2cv9ffvaqme92skvdppq8s92t3qai7t.apps.googleusercontent.com',
+      clientSecret: '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',
+      callbackURL: 'http://localhost:3001/api/google-login/callback',
+      scope: ['profile', 'email']
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      // Gérer les informations du profil utilisateur ici
+      // Sauvegarder les informations dans la base de données si nécessaire
+      // Appeler la fonction `done` avec les informations du profil utilisateur
+      done(null, profile);
+    }
+  )
+);
+
+// Route for Google authentication
+app.get('/api/google-login', passport.authenticate('google'));
+
+// Callback after Google authentication
+app.get(
+  '/api/google-login/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    // Rediriger vers la page d'inscription sur le frontend
+    res.redirect('http://localhost:8080/chat');
+  }
+);
 
 // Define a schema for the inscription
 const inscriptionSchema = new mongoose.Schema({
@@ -53,6 +125,33 @@ app.post('/api/webhook', (req, res) => {
   res.status(200).json({ status: 'success' });
 });
 
+// Serve the chat.html file
+app.use(express.static(path.join(__dirname, '../public')));
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:8080',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  // Événement de réception de message
+  socket.on('message', (data) => {
+    console.log('Message received:', data);
+    // Envoyer le message à tous les clients connectés (broadcast)
+    io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
 const PORT = 3001;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- backend2/index.js
+++ backend2/index.js
@@ -2,14 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { google } = require('googleapis');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Add the Permissions-Policy header middleware
+app.use((req, res, next) => {
+  res.setHeader('Permissions-Policy', 'ch-ua-form-factor');
+  next();
+});
+
 // Connect to MongoDB
 mongoose
-  .connect('mongodb+srv://alinaghani13:herwob-pewcak-qytKi8@cluster0.z7tc2lc.mongodb.net/mail_service', { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect('mongodb+srv://alinaghani13:herwob-pewcak-qytKi8@cluster0.z7tc2lc.mongodb.net/mail_service', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
@@ -38,6 +48,94 @@ app.post('/api/inscriptions', async (req, res) => {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+});
+
+// API endpoint for the webhook
+app.post('/api/webhook', (req, res) => {
+  const { from, to, subject, message } = req.body;
+
+  // Process the webhook data as desired (e.g., save it to the database)
+  // ...
+
+  res.status(200).json({ status: 'success' });
+});
+
+// Google authentication setup
+const credentials = {
+  client_id: '935153154949-h2cv9ffvaqme92skvdppq8s92t3qai7t.apps.googleusercontent.com',
+  client_secret: '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',
+  redirect_uris: ['http://localhost:3001/api/google-login/callback']
+};
+
+const oAuth2Client = new google.auth.OAuth2(
+  credentials.client_id,
+  credentials.client_secret,
+  credentials.redirect_uris[0]
+);
+
+// Authorize access to Google Calendar
+function authorize(callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: ['https://www.googleapis.com/auth/calendar.readonly']
+  });
+
+  callback(authUrl);
+}
+
+// Get upcoming events from user's Google Calendar
+function getUpcomingEvents(accessToken, callback) {
+  oAuth2Client.setCredentials({ access_token: accessToken });
+
+  const calendar = google.calendar({ version: 'v3', auth: oAuth2Client });
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  calendar.events.list(
+    {
+      calendarId: 'primary',
+      timeMin: today.toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime'
+    },
+    (err, res) => {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      }
+
+      const events = res.data.items;
+      callback(events);
+    }
+  );
+}
+
+// Route for Google authentication
+app.get('/api/google-login', (req, res) => {
+  authorize(authUrl => {
+    res.redirect(authUrl);
+  });
+});
+
+// Callback after Google authentication
+app.get('/api/google-login/callback', (req, res) => {
+  const { code } = req.query;
+
+  oAuth2Client.getToken(code, (err, token) => {
+    if (err) {
+      console.error('Error retrieving access token', err);
+      res.status(500).json({ error: 'Failed to retrieve access token' });
+      return;
+    }
+
+    const { access_token } = token;
+
+    getUpcomingEvents(access_token, events => {
+      res.json(events);
+    });
+  });
 });
 
 const PORT = 3002;
